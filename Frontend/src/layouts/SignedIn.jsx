f0f517b6733014e43d3f821244eb09f762daa2a2
kenanyasinsarigul <kenansarigul97@gmail.com>
2021-08-31 16:50:03 +0300

    Java_React_Camp_Northwind

---
+++ Frontend/src/layouts/SignedIn.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Dropdown, Menu,Image } from 'semantic-ui-react'
+
+export default function SignedIn({signOut}) {
+    return (
+        <div>
+            <Menu.Item>
+                <Image avatar spaced="right" src="https://pbs.twimg.com/profile_images/1404479261566713856/_MklDkhx_400x400.jpg"/>
+                <Dropdown pointing="top left" text="Engin">
+                    <Dropdown.Menu>
+                        <Dropdown.Item text="Bilgilerim" icon="info"/>
+                        <Dropdown.Item onClick={signOut} text="Çıkış Yap" icon="sign-out"/>
+                    </Dropdown.Menu>
+                </Dropdown>
+            </Menu.Item>
+        </div>
+    )
+}
